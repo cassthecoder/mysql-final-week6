eecc7cfe13b4ad5624970b42b360044464fab818
Cassandra Matos <cassandra@matosmendez.com>
2021-08-04 19:26:39 -0700

    set up main method

---
+++ src/com/cassandramatos/week5/App.java
@@ -0,0 +1,10 @@
+package com.cassandramatos.week5;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing changes.");
+	}
+
+}
